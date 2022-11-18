d264cb28d377acac513ad6c8d706fed02b1b21f9
Elias Feijó <eliasfeijo.dev@gmail.com>
2022-11-18 14:51:31 -0300

    feat: General API annotation swagger

--- server/web.go
+++ server/web.go
@@ -9,6 +9,22 @@ import (
 	"gitlab.com/go-api-boilerplate/routes"
 )
 
+// @title          API
+// @version        1.0
+// @description    A basic API containing endpoints for account creation and authentication
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name  API Support
+// @contact.url   http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url  http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host     localhost:8080
+// @BasePath /v1
+
+// @securityDefinitions.apikey AuthorizationBearer
 func Run() (err error) {
 	cfg := config.GetConfig()
 
