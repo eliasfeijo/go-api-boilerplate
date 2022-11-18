e375b510412eca89defa71e2c627813c7d3faea6
Elias Feijó <eliasfeijo.dev@gmail.com>
2022-11-18 16:49:11 -0300

    feat: removing ID from payloads

--- payload/v1/account.go
+++ payload/v1/account.go
@@ -3,7 +3,6 @@ package v1
 import "gitlab.com/go-api-boilerplate/dto"
 
 type Account struct {
-	ID       string `json:"id,omitempty"`
 	Email    string `json:"email,omitempty"`
 	Password string `json:"password,omitempty"`
 	User     *User  `json:"user,omitempty"`
@@ -15,7 +14,6 @@ func AccountToDTO(account *Account) *dto.Account {
 		user = UserToDTO(account.User)
 	}
 	return &dto.Account{
-		ID:       account.ID,
 		Email:    account.Email,
 		Password: account.Password,
 		User:     user,
--- payload/v1/user.go
+++ payload/v1/user.go
@@ -3,13 +3,11 @@ package v1
 import "gitlab.com/go-api-boilerplate/dto"
 
 type User struct {
-	ID   string `json:"id,omitempty"`
 	Name string `json:"name,omitempty"`
 }
 
 func UserToDTO(user *User) *dto.User {
 	return &dto.User{
-		ID:   user.ID,
 		Name: user.Name,
 	}
 }
